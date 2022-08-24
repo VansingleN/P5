5c348f2160122125a8c66934a27aa7bde957e184
Nicolas <vansingle.n@gmail.com>
2022-08-24 15:41:50 +0200

    Refactor

---
+++ front/js/index.js
@@ -0,0 +1,43 @@
+function getProductsFromBackend() {
+      
+      fetch('http://localhost:3000/api/products')
+            .then((res) => res.json())
+            .then((res) => showProducts(res))
+}
+
+function displayProduct(product) {
+
+      const items = document.querySelector("#items")
+      const id = product._id
+      const anchor = document.createElement("a")
+      anchor.href = "./product.html?id=" + id
+      const article = document.createElement("article")
+      const imageUrl = product.imageUrl
+      const image = document.createElement("img")
+      image.src = imageUrl
+      image.alt = product.altTxt
+      const title = document.createElement("h3")
+      title.innerHTML = product.name
+      title.classList = "productName"
+      const paragraph = document.createElement("p")
+      paragraph.classList = "productDescription"
+      paragraph.innerHTML = product.description
+
+      items.appendChild(anchor)
+      anchor.appendChild(article)
+      article.appendChild(image)
+      article.appendChild(title)
+      article.appendChild(paragraph)
+}
+
+function showProducts(products) {
+
+      for (product of products) {
+            displayProduct(product)
+      }
+}
+
+getProductsFromBackend()
+
+
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,20 +1,22 @@
-const id = window.location.search
-const urlSearchParams = new URLSearchParams(id)
-const paramId = urlSearchParams.get("id")
+function getDataFromId() {
 
-fetch(`http://localhost:3000/api/products/${paramId}`)
-.then((res) => res.json())
-.then((res) => showImage(res))
-.then(showTitleAndPrice)
-.then(showDescription)
-.then(showColors)
+    const id = window.location.search
+    const urlSearchParams = new URLSearchParams(id)
+    const paramId = urlSearchParams.get("id")
+    fetch(`http://localhost:3000/api/products/${paramId}`)
+        .then((res) => res.json())
+        .then((res) => showImage(res))
+        .then(showTitleAndPrice)
+        .then(showDescription)
+        .then(showColors)
+}
 
 function showImage(res) {
 
     const item__img = document.querySelector(".item__img");
     const image = document.createElement("img")
-          image.src = res.imageUrl
-          image.alt = res.altTxt
+    image.src = res.imageUrl
+    image.alt = res.altTxt
     item__img.appendChild(image)
     return res
 }
@@ -22,27 +24,28 @@ function showImage(res) {
 function showTitleAndPrice(res) {
 
     const title = document.querySelector("#title")
-          title.innerHTML = res.name
+    title.innerHTML = res.name
     const price = document.querySelector("#price")
-          price.innerHTML = res.price
+    price.innerHTML = res.price
     return res
 }
-    
+
 function showDescription(res) {
 
     const description = document.querySelector("#description")
-          description.innerHTML = res.description
+    description.innerHTML = res.description
     return res
 }
 
 function showColors(res) {
 
     res.colors.forEach(color => {
-
         const colors = document.querySelector("#colors")
         const option = document.createElement("option")
-              option.value = color
-              option.text = color
+        option.value = color
+        option.text = color
         colors.appendChild(option)
-    })  
+    })
 }
+
+getDataFromId()
